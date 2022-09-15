363f266bff378391ab77c6d7612817f149d5bb6b
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2022-09-15 16:05:54 +0200

    change zerodist to use singular values

--- experiments/experiment_zerodist.py
+++ experiments/experiment_zerodist.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
-from gltseq.toeplitz import eigsum_from_matrix
+from gltseq.toeplitz import singsum_from_matrix
 
 
 N = 100
@@ -11,19 +11,19 @@ eigsums_z = np.empty(N-3)
 ns = np.arange(4, N+1)
 for n in ns:
     R = np.zeros((n, n))
-    N = np.full((n, n), 1/np.sqrt(n))
+    N = np.diag(np.repeat(1/np.sqrt(n), n))
     R[0:1, 0:1] = 9
-    eigsums_sr[n-4] = eigsum_from_matrix(R)
-    eigsums_sn[n-4] = eigsum_from_matrix(N)
-    eigsums_z[n-4] = eigsum_from_matrix(R + N)
+    eigsums_sr[n-4] = singsum_from_matrix(R)
+    eigsums_sn[n-4] = singsum_from_matrix(N)
+    eigsums_z[n-4] = singsum_from_matrix(R + N)
     
 
 f = plt.figure()
 ax: plt.Axes  = f.add_subplot(111)
-ax.set_title('Average Eigenvalues of Zero-Distributed Matrix')
+# ax.set_title('Average Singular Values of Zero-Distributed Matrix')
 ax.set_xlabel('$n$')
-ax.set_ylabel(r'$\bar{\lambda}$')
-ax.plot(ns, eigsums_sr, ns, eigsums_sn, ns, eigsums_z)
-ax.legend([r'$R = [9]_{i,j=1}^2$', r'N = $[\frac{1}{\sqrt{n}}]$', '$Z = R + N$'])
+ax.set_ylabel(r'$\bar{\sigma}$')
+ax.plot(ns, eigsums_sr, ns, eigsums_sn, '--', ns, eigsums_z, '.')
+ax.legend([r'$R = [9]_{i,j=1}^2$', r'N = $\frac{1}{\sqrt{n}} I$', '$Z = R + N$'])
 plt.show()
 plt.close(f)
