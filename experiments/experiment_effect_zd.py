5340dbc31b6e0167ac7711ca2f746e0bce04141b
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2022-09-17 15:41:36 +0200

    add saving of figures to experiments

--- experiments/experiment_effect_zd.py
+++ experiments/experiment_effect_zd.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
-from gltseq.toeplitz import eigsum_from_matrix
+from gltseq.toeplitz import singsum_from_matrix
 
 
 N = 100
 MIN_N = 2
-eigsums_a = np.empty(N-MIN_N+1)
-eigsums_b = np.empty(N-MIN_N+1)
-eigsums_z = np.empty(N-MIN_N+1)
-eigdiffs_ab = np.empty(N-MIN_N+1)
+singsums_a = np.empty(N-MIN_N+1)
+singsums_b = np.empty(N-MIN_N+1)
+singsums_z = np.empty(N-MIN_N+1)
+singdiffs_ab = np.empty(N-MIN_N+1)
 ns = np.arange(MIN_N, N+1)
 for n in ns:
     B = \
@@ -20,19 +20,19 @@ for n in ns:
     N = np.full((n, n), 1/n)
     A = B + R + N
     R[0:1, 0:1] = 9
-    eigsums_a[n-MIN_N] = eigsum_from_matrix(A)
-    eigsums_b[n-MIN_N] = eigsum_from_matrix(B)
-    eigsums_z[n-MIN_N] = eigsum_from_matrix(R + N)
-    eigdiffs_ab[n-MIN_N] = np.average(np.linalg.eig(A)[0] - np.linalg.eig(B)[0])
+    singsums_a[n-MIN_N] = singsum_from_matrix(A)
+    singsums_b[n-MIN_N] = singsum_from_matrix(B)
+    singsums_z[n-MIN_N] = singsum_from_matrix(R + N)
+    singdiffs_ab[n-MIN_N] = np.average(np.linalg.svd(A)[1] - np.linalg.svd(B)[1])
 
 f = plt.figure()
 ax: plt.Axes  = f.add_subplot(111)
-ax.set_title('Average Eigenvalues of Disturbed Matrix')
+#ax.set_title('Average Singular Values of Perturbed Matrix')
 ax.set_xlabel('$n$')
-ax.set_ylabel(r'$\bar{\lambda}$')
-ax.plot(ns, eigsums_a, '--', ns, eigsums_b, '.', ns, eigsums_z, '.', ns, eigdiffs_ab)
-ax.legend([r'$A = B+Z$', r'$B = toeplitz(1, 3, 1)$', '$Z = R + N$', 'ev diff of A, B'])
+ax.set_ylabel(r'$\bar{\sigma}$')
+ax.plot(ns, singsums_a, '--', ns, singsums_b, '.', ns, singsums_z, '.', ns, singdiffs_ab)
+ax.legend([r'$A = B+Z$', r'$B = toeplitz(1, 3, 1)$', '$Z = R + N$', 'sv diff of A, B'])
 ax.set_xlim(0)
 ax.set_ylim(0)
 plt.show()
-plt.close(f)
+f.savefig("media/effect_zd.png", dpi=600)
